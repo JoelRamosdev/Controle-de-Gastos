ee55170b7f785b8d939232d67e0ab663d6b1b04d
Joel Ramos <joelramos23@gmail.com>
2023-02-02 20:46:10 +0000

    Starting JS code

---
+++ app.js
@@ -0,0 +1,15 @@
+// importar funções da livraria  do code.org para serem usadas na aplicação
+import {
+    onEvent, 
+    showElement, 
+    setScreen} from './lib/code.org.js';
+
+// definir abaixo quais as variáveis que serão usadas
+
+
+// perguntar ao usuário quanto ele possui na carteira
+let carteira = prompt("QUANTO VOCÊ POSSUI NA CARTEIRA?"); 
+
+
+// perguntar ao usuario quanto ele quer economizar 
+let economia = prompt("QUANTO VOCÊ QUER ECONOMIZAR?");
