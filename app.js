38bac079fccd73c6320d8682502497200818666e
Joel Ramos <joelramos23@gmail.com>
2023-02-09 10:40:51 +0000

    atualizando

--- app.js
+++ app.js
@@ -3,14 +3,7 @@ import {
     onEvent, 
     showElement, 
     setScreen} 
-    from './lib/code.org.js';
-
-    import {
-    imprimeExtrato
-    } 
-    from './gui.js';
-
-// definir abaixo quais as variáveis que serão usadas
+    from './lib/code.org.js';  
 
 
 // perguntar ao usuário quanto ele possui na carteira
@@ -29,9 +22,6 @@ const COMIDA = 18.00;
 const CIRCO = 15.00; 
 const BRINQUEDO = 13.00; 
 
-// criar variavel de extrato que será alimentada conforme os gastos forem acontecendo
-let extractList = []
-
 // incluir o valor informado para gastos na carteira
 document.querySelector("#wallet").innerHTML = carteira;
 
@@ -40,24 +30,21 @@ onEvent("comida","click", () =>{
     carteira -= COMIDA; // reduzir o valor que foi declarado na variável
     alert("Você gastou R$" + COMIDA + " com comida!"); // informar ao usuário que ouve uma transação
     extrato += "R$" + COMIDA + " Gastos com Comida \n"; // armazenar em extrato o que está sendo gasto
-    alteraCarteiraEalertaLimite();  
-    atualizaListaComida(); 
+    
 });    
 
 onEvent("brinquedo","click", () => {
     carteira -= BRINQUEDO;
     alert("Você gastou R$" + BRINQUEDO + " com brinquedo!");    
     extrato += "R$" + BRINQUEDO + " Gastos com Brinquedo \n"; 
-    alteraCarteiraEalertaLimite();   
-    atualizaListaBrinquedo();
+    
 });
 
 onEvent("circo","click", () => {
     carteira -= CIRCO; 
     alert("Você gastou R$" + CIRCO + " com circo!");    
     extrato += "R$" + CIRCO + " Gastos com Circo \n"; 
-    alteraCarteiraEalertaLimite();
-    atualizaListaCirco();
+    
 });
 
 // quando clicar no botão EXTRATO ele vai abrir uma caixa de alerta listando os gastos e informando quando tem de saldo e qual o valor definido como economia.
@@ -70,34 +57,7 @@ onEvent("go-extrato", "click", () => {
 // criar a variavel que altera o background do BODY para vermelho
 const walletColorRed = () => {
     document.querySelector("body").style.background= "linear-gradient(to left, #660000, #990000, #ff0000)";   
- }
-
- function atualizaListaBrinquedo() {
-    let transaction = {
-        nome: "brinquedo",
-        valor: 13.00,
-        data: new Date()
-    };
-    extractList.push(transaction);
-}
-
-function atualizaListaComida() {
-    let transaction = {
-        nome: "comida",
-        valor: 18.00,
-        data: new Date()
-    };
-    extractList.push(transaction);
-}
-
-function atualizaListaCirco() {
-    let transaction = {
-        nome: "circo",
-        valor: 15.00,
-        data: new Date()
-    };
-    extractList.push(transaction);
-}
+ } 
 
  function alteraCarteiraEalertaLimite() {
     document.querySelector("#wallet").innerHTML = carteira; // apresentar o valor restante em cateria deduzido do gasto
